cf8bc91c3c69fe80303f0a2837e4737c9b07c5a7
daniela-mattos <kittybhank@hotmail.com>
2024-06-11 14:44:10 -0300

    iniciando projeto

---
+++ src/test/java/danimattos/forumhub_api/ForumhubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package danimattos.forumhub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumhubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
